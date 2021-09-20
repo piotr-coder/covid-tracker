1ebeeac611409e64859390aabd5c8cc1cba3cc95
piotr-coder <55543334+piotr-coder@users.noreply.github.com>
2021-09-20 12:10:57 +0200

    Update ApiController.java

--- src/main/java/spring/covidtracker/controller/ApiController.java
+++ src/main/java/spring/covidtracker/controller/ApiController.java
@@ -2,6 +2,7 @@ package spring.covidtracker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.covidtracker.model.LocationStats;
